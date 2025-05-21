43fb7be152d183b7dccf54ffd424a0f1d7cd7771
vinh204 <vinh204.blog@gmail.com>
2025-05-21 21:30:15 +0700

    Create env and gitignore

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const route = require('./routes/client/index.route');
+require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.set('views', './views');
 app.set('view engine', 'pug');
